4e48caf4559ab7db9794978216c1e5c44c01051b
Volodya Ismailyan <nenie_iri@mail.ru>
2022-10-29 15:19:33 +0400

    trimming config file

--- Srcs/file_open_read.cpp
+++ Srcs/file_open_read.cpp
@@ -1,23 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   file_open.cpp                                      :+:      :+:    :+:   */
+/*   file_open_read.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:47:46 by vismaily          #+#    #+#             */
-/*   Updated: 2022/10/28 17:07:34 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/10/29 14:58:29 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "webserv.hpp"
+#include "webserv.hpp"
+
+static void	config_read_trim(std::ifstream &config_stream, std::string &config)
+{
+	std::string::size_type	found;
+	std::ostringstream		ss;
+
+	ss << config_stream.rdbuf();
+	config = ss.str();
+	found = config.find_last_not_of(" \t\v\r\n\f");
+	if (found != std::string::npos)
+		config = config.substr(0, found + 1);
+	found = config.find_first_not_of(" \t\v\r\n\f");
+	if (found == std::string::npos)
+		config = "";
+	else if (found != 0)
+		config = config.substr(found);
+}
 
 int	file_open_read(std::string config_file, std::string &config)
 {
 	std::string::size_type	found;
 	std::string				ext;
 	std::ifstream			config_stream;
-	std::ostringstream		ss;
 
 	ext = CONFIG_EXTANSION;
 	found = config_file.rfind(CONFIG_EXTANSION);
@@ -37,8 +53,7 @@ int	file_open_read(std::string config_file, std::string &config)
 			std::cout << "Error: Failed to open the file." << std::endl;
 			return (-1);
 		}
-		ss << config_stream.rdbuf();
-		config= ss.str();
+		config_read_trim(config_stream, config);
 		config_stream.close();
 	}
 	return (0);
--- Srcs/main.cpp
+++ Srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 13:17:05 by vismaily          #+#    #+#             */
-/*   Updated: 2022/10/28 17:07:28 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/10/29 15:00:37 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 int	main(int argc, char **argv)
 {
-	int			is_open;
+	int			res;
 	std::string	config;
 
 	if (argc == 1)
-		is_open = file_open_read(CONFIG_DEFAULT, config);
+		res = file_open_read(CONFIG_DEFAULT, config);
 	else if (argc == 2)
-		is_open = file_open(argv[1], config);
+		res = file_open_read(argv[1], config);
 	else
 	{
 		std::cout << "Error: Argumets count must be 1 or 0." << std::endl;
 		return (0);
 	}
-	if (is_open != -1)
-		std::cout << config << std::endl;
-	//	start_server();
+//	if (res != -1)
+//		start_server(config);
 	return (0);
 }
