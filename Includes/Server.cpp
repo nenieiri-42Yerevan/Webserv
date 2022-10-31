f4d0cc40b44098d26fbecec40154b7fa698ec35d
Volodya Ismailyan <nenie_iri@mail.ru>
2022-10-31 17:14:19 +0400

    little change in Server class parsing part

--- Includes/Server.cpp
+++ Includes/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 16:42:16 by vismaily          #+#    #+#             */
-/*   Updated: 2022/10/31 14:07:32 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/10/31 16:58:08 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ Server::Server(std::string &body)
 	std::string	tmp;
 
 	tmp = body.substr(1, body.length() - 2);
+	if (tmp == "" || tmp.find_first_not_of(" \t\v\r\n\f") == std::string::npos)
+		throw std::logic_error("Error: Config file: directive 'server' is empty.");
 	parsingBody(tmp);
 }
 
@@ -63,21 +65,44 @@ const std::vector<std::string>	Server::getServerName() const
 void	Server::parsingBody(std::string &body)
 {
 	typedef std::string	str;
-	str::size_type		found_begin;
-	str::size_type		found_end;
+	str::size_type		found_begin_name;
+	str::size_type		found_end_name;
+	str::size_type		found_begin_value;
+	str::size_type		found_end_value;
+	std::string			tmp;
 
-	if (body == "" || body.find_first_not_of(" \t\v\r\n\f") == str::npos)
-		throw std::logic_error("Error: Config file: directive 'server' is empty.");
 	while (body != "")
 	{
-		found_begin = body.find_first_not_of(" \t\v\r\n\f");
-		if (found_begin == str::npos)
+		found_begin_name = body.find_first_not_of(" \t\v\r\n\f");
+		if (found_begin_name == str::npos)
 			break ;
-		found_end = body.find_first_of(" \t\v\r\n\f", found_begin);
-		if (found_end == str::npos)
+		found_end_name = body.find_first_of(" \t\v\r\n\f", found_begin_name);
+		if (found_end_name == str::npos)
 			throw std::logic_error("Error: Config file: after directive name " \
 								   "must be at least one whitespce and " \
 								   "after that must be its value.");
-		body.erase(0, found_end);
+		found_begin_value = body.find_first_not_of(" \t\v\r\n\f", found_end_name);
+		if (found_begin_value == str::npos)
+		{
+			tmp = "Error: Config file: directive '";
+			tmp += body.substr(found_begin_name, found_end_name - found_begin_name);
+			tmp += "' doesn't have a value.";
+			throw std::logic_error(tmp);
+		}
+		if (body.compare(found_begin_name, 8, "location") != 0)
+		{
+			found_end_value = body.find_first_of(';', found_begin_value);
+			if (found_end_value == str::npos)
+			{
+				tmp = "Error: Config file: value of directive '";
+				tmp += body.substr(found_begin_name, found_end_name - found_begin_name);
+				tmp += "' must end with ';' symbol.";
+				throw std::logic_error(tmp);
+			}
+		}
+		else
+		{
+		}
+		body.erase(0, found_end_value + 1);
 	}
 }
