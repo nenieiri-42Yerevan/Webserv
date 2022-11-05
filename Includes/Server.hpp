02275def90aed57f110c948c9bd38af27c125edf
Volodya Ismailyan <nenie_iri@mail.ru>
2022-11-05 13:10:22 +0400

    norminette 2

--- Includes/Server.hpp
+++ Includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 16:36:35 by vismaily          #+#    #+#             */
-/*   Updated: 2022/11/02 15:47:09 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/11/05 12:48:00 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ class	Server
 {
 	private:
 		typedef std::string				t_str;
+
 	public:
 		Server(t_str &body);
 		Server(const Server &other);
@@ -35,24 +36,24 @@ class	Server
 		Server();
 
 	public:
-		const std::vector<std::string>	&getServerName() const;
+		const std::vector<t_str>		&getServerName() const;
 		const std::map<t_str, t_str>	&getListen() const;
 	private:
-		void		setServerName(t_str &value);
-		void		setListen(t_str &addr, t_str &port);
+		void					setServerName(t_str &value);
+		void					setListen(t_str &addr, t_str &port);
 	private:
-		void		parsingListen(t_str &value);
-		bool		isValidPort(t_str port) const;
-		bool		isValidIP(t_str addr) const;
-		void		setFildes(const t_str &name, t_str &value);
-		void		parsingLocation(t_str &body, t_str::size_type value_begin, \
-												t_str::size_type value_end);
-		void		setDefaults();
-		void		parsingBody(t_str &body);
+		void					parsingListen(t_str &value);
+		bool					isValidPort(t_str port) const;
+		bool					isValidIP(t_str addr) const;
+		void					setFildes(const t_str &name, t_str &value);
+		void					parsingLocation(t_str &body, t_str::size_type \
+									value_begin, t_str::size_type value_end);
+		void					setDefaults();
+		void					parsingBody(t_str &body);
 	private:
-		std::vector<t_str>				_directiveList;
-		std::vector<t_str>				_serverName;
-		std::map<t_str, t_str>			_listen;
+		std::vector<t_str>		_directiveList;
+		std::vector<t_str>		_serverName;
+		std::map<t_str, t_str>	_listen;
 };
 
 #endif
