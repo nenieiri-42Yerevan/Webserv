7e10ba7f8e6ed53620c394855a9b2ac03b8bff4e
Volodya Ismailyan <nenie_iri@mail.ru>
2022-11-06 12:37:11 +0400

    norminette

--- Includes/HttpServer.cpp
+++ Includes/HttpServer.cpp
@@ -12,22 +12,22 @@ HttpServer::~HttpServer()
 
 HttpServer::HttpServer(const HttpServer &other)
 {
-    *this = other;
+	*this = other;
 }
 
 HttpServer &HttpServer::operator=(const HttpServer &other)
 {
-    if (this != &other)
-    {
-        this->acceptfds = other.acceptfds;
-        this->listenSockets = other.listenSockets;
-    }
-    return (*this);
+	if (this != &other)
+	{
+		this->acceptfds = other.acceptfds;
+		this->listenSockets = other.listenSockets;
+	}
+	return (*this);
 }
 
 HttpServer::HttpServer(std::vector<Server> *vec)
 {
-    this->vec = *vec;
+	this->vec = *vec;
 }
 
 void HttpServer::createSockets(int i)
@@ -35,24 +35,24 @@ void HttpServer::createSockets(int i)
     struct sockaddr_in	address;
 	int					on = 1;
 
-    this->listenSockets[i].sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (this->listenSockets[i].sockfd < 0)
-        throw std::runtime_error("Error: when creating a socket.");
-    if (setsockopt(this->listenSockets[i].sockfd, SOL_SOCKET, \
-				SO_REUSEADDR | SO_REUSEPORT, &on, sizeof(int)) < 0)
-    {
-            close(this->listenSockets[i].sockfd);
-            throw std::runtime_error("Error: setsockopt");
-    }
-    memset((char *)&address, 0, sizeof(address));
+	this->listenSockets[i].sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (this->listenSockets[i].sockfd < 0)
+		throw std::runtime_error("Error: when creating a socket.");
+	if (setsockopt(this->listenSockets[i].sockfd, SOL_SOCKET, \
+				SO_REUSEADDR | SO_REUSEPORT, &on, sizeof(on)) < 0)
+	{
+		close(this->listenSockets[i].sockfd);
+		throw std::runtime_error("Error: setsockopt");
+	}
+	memset((char *)&address, 0, sizeof(address));
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr(this->listenSockets[i].host.c_str());
 	address.sin_port = htons(this->listenSockets[i].port);
-    if (bind(this->listenSockets[i].sockfd, (struct sockaddr *) &address, \
+	if (bind(this->listenSockets[i].sockfd, (struct sockaddr *) &address, \
 					sizeof(address)) < 0)
-        throw std::runtime_error("Error: binding socket.");
-    if (listen(this->listenSockets[i].sockfd, 32) < 0)
-       throw std::runtime_error("Error: listening socket.");
+		throw std::runtime_error("Error: binding socket.");
+	if (listen(this->listenSockets[i].sockfd, 32) < 0)
+		throw std::runtime_error("Error: listening socket.");
 }
 
 void HttpServer::createListen()
@@ -64,8 +64,8 @@ void HttpServer::createListen()
     c = 0;
     for (size_t i = 0; i < this->vec.size(); ++i)
     {
-        std::map<std::string, std::string>::const_iterator it = vec[i].getListen().begin();
-        while (it != vec[i].getListen().end())
+		std::map<std::string, std::string>::const_iterator it = vec[i].getListen().begin();
+		while (it != vec[i].getListen().end())
         {
             this->listenSockets[c].host = it->first;
             this->listenSockets[c].port = atoi(it->second.c_str());
@@ -78,31 +78,28 @@ void HttpServer::createListen()
 
 void HttpServer::createacceptfd(int i, fd_set initrset, int *maxfd)
 {
-     struct sockaddr_in	address;
-     int fd;
-     socklen_t addrlen;
-     
-     if ((fd = accept(this->listenSockets[i].sockfd,(struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
-    {
-        throw std::runtime_error("Error: accept");
-    }
-    fcntl(fd, F_SETFL, O_NONBLOCK);
-    FD_SET(fd, &initrset);
-    (*maxfd)++;
-    this->acceptfds.push_back(fd);
+	struct sockaddr_in	address;
+	int					fd;
+	socklen_t			addrlen;
+
+	if ((fd = accept(this->listenSockets[i].sockfd, (struct sockaddr *)&address, \
+					(socklen_t*)&addrlen)) < 0)
+		throw std::runtime_error("Error: accept");
+	fcntl(fd, F_SETFL, O_NONBLOCK);
+	FD_SET(fd, &initrset);
+	(*maxfd)++;
+	this->acceptfds.push_back(fd);
 }
 
 void HttpServer::getrequest(int i)
 {
-    char buffer[1024];
-    int n = 0;
+    char	buffer[1024];
+    int		n = 0;
 
-    if((n = recv(this->acceptfds[i], buffer, sizeof(buffer) - 1, 0)) < 0)
-    {
-        throw std::runtime_error("Error: recieve");
-    }
-    buffer[n] = '\0';
-    std::cout << buffer << std::endl;
+	if ((n = recv(this->acceptfds[i], buffer, sizeof(buffer) - 1, 0)) < 0)
+		throw std::runtime_error("Error: recieve");
+	buffer[n] = '\0';
+	std::cout << buffer << std::endl;
 }
 
 
