fa873b458abd00f3ad66c14d9bf6911e53f7468d
Volodya Ismailyan <nenie_iri@mail.ru>
2022-11-07 15:01:56 +0400

    add parsing for multiple lines in header

--- Includes/Client.cpp
+++ Includes/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 16:38:07 by vismaily          #+#    #+#             */
-/*   Updated: 2022/11/07 13:51:27 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/11/07 14:56:58 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,21 @@
 
 Client::Client()
 {
+	this->_request = "";
+	this->_body = "";
 	this->_isStart = 0;
+	this->_isHeader = 0;
+	this->_lastHeader = "";
 }
 
 Client::Client(const Client &other)
 {
 	this->_request = other._request;
+	this->_header = other._header;
+	this->_body = other._body;
+	this->_isStart = other._isStart;
+	this->_isHeader = other._isHeader;
+	this->_lastHeader = other._lastHeader;
 }
 
 Client	&Client::operator=(const Client &rhs)
@@ -31,6 +40,11 @@ Client	&Client::operator=(const Client &rhs)
 	if (this != &rhs)
 	{
 		this->_request = rhs._request;
+		this->_header = rhs._header;
+		this->_body = rhs._body;
+		this->_isStart = other._isStart;
+		this->_isHeader = other._isHeader;
+		this->_lastHeader = other._lastHeader;
 	}
 	return (*this);
 }
@@ -68,27 +82,44 @@ void	Client::parsingHeader(std::string line)
 	std::string				header_value;
 
 	/* if start with space */
-	colon = line.find(":");
-	if (colon != std::string::npos)
+	begin = line.find_first_not_of(" \t\v\r\n\f");
+	if (begin != 0)
 	{
-		begin = line.find_first_not_of(" \t\v\r\n\f");
-		end = line.find_last_not_of(" \t\v\r\n\f", colon - 1);
-		if (begin == std::string::npos || end == std::string::npos)
-			return ;
-		end += 1;
-		header_name = line.substr(begin, end - begin);
-		line = line.substr(colon + 1, line.length() - (colon + 1));
-		begin = line.find_first_not_of(" \t\v\r\n\f");
 		end = line.find_last_not_of(" \t\v\r\n\f", line.length() - 1);
-		if (begin == std::string::npos || end == std::string::npos)
+		if (end == std::string::npos)
 			return ;
 		end += 1;
 		header_value = line.substr(begin, end - begin);
-		/*
-		 * name to lowercase
-		 * duplicate values
-		 */
-		this->_header.insert(std::make_pair(header_name, header_value));
+		if (this->_lastHeader != "")
+		{
+			this->_header[_lastHeader] += " ";
+			this->_header[_lastHeader] += header_value;
+		}
+	}
+	else
+	{
+		colon = line.find(":");
+		if (colon != std::string::npos)
+		{
+			begin = 0;
+			end = line.find_last_not_of(" \t\v\r\n\f", colon - 1);
+			if (begin == std::string::npos || end == std::string::npos)
+				return ;
+			end += 1;
+			header_name = line.substr(begin, end - begin);
+			line = line.substr(colon + 1, line.length() - (colon + 1));
+			begin = line.find_first_not_of(" \t\v\r\n\f");
+			end = line.find_last_not_of(" \t\v\r\n\f", line.length() - 1);
+			if (begin == std::string::npos || end == std::string::npos)
+				return ;
+			end += 1;
+			header_value = line.substr(begin, end - begin);
+			/*
+			 * name to lowercase
+			 */
+			this->_header.insert(std::make_pair(header_name, header_value));
+			this->_lastHeader = header_name;
+		}
 	}
 }
 
--- Includes/Client.hpp
+++ Includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 16:38:11 by vismaily          #+#    #+#             */
-/*   Updated: 2022/11/07 13:47:31 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/11/07 14:41:51 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ class	Client
 	private:
 		int									_isStart;
 		int									_isHeader;
+		std::string							_lastHeader;
 };
 
 #endif
