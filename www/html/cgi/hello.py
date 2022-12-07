5c471e23dd545d12a45cdf588a70890eb8b779b7
Ara Stepanyan <arastepa@c2r14s5.42yerevan.am>
2022-12-07 15:29:56 +0400

    ..

--- www/html/cgi/hello.py
+++ www/html/cgi/hello.py
@@ -7,4 +7,8 @@ first_name = form.getvalue('fname')
 last_name  = form.getvalue('lname')
 
 
-print ("""<html><body><p>your name is : %s, %s</p></body></html>"""%(first_name, last_name));
+if first_name is None:
+    first_name = "you haven't wriiten text"
+if last_name is None:
+    last_name = "you haven't wriiten text"
+print ("<html><body><p>your name is : %s, %s</p></body></html>"%(first_name, last_name));
